1be818cd1487bf2fd30db957c5260699f0135e3c
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-04-11 00:58:26 -0400

    added ranking feature

--- questions_template/js/scrolling-nav.js
+++ questions_template/js/scrolling-nav.js
@@ -27,3 +27,43 @@
   });
 
 })(jQuery); // End of use strict
+
+var el = document.getElementById('items');
+var sortable = Sortable.create(el);
+
+// Simple list
+var list = document.getElementById("my-ui-list");
+Sortable.create(list); // That's all.
+
+
+// Grouping
+var foo = document.getElementById("foo");
+Sortable.create(foo, { group: "omega" });
+
+var bar = document.getElementById("bar");
+Sortable.create(bar, { group: "omega" });
+
+
+// Or
+var container = document.getElementById("multi");
+var sort = Sortable.create(container, {
+  animation: 150, // ms, animation speed moving items when sorting, `0` — without animation
+  handle: ".tile__title", // Restricts sort start click/touch to the specified element
+  draggable: ".tile", // Specifies which items inside the element should be sortable
+  onUpdate: function (evt/**Event*/){
+     var item = evt.item; // the current dragged HTMLElement
+  }
+});
+
+// ..
+sort.destroy();
+
+
+// Editable list
+var editableList = Sortable.create(editable, {
+  filter: '.js-remove',
+  onFilter: function (evt) {
+    var el = editableList.closest(evt.item); // get dragged item
+    el && el.parentNode.removeChild(el);
+  }
+});
